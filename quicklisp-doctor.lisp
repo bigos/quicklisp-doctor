22eb968619a88c69c8860704c134ea24b6705ea5
Jacek Podkanski <ruby.object@googlemail.com>
2022-10-29 23:10:27 +0100

    some tidying up

--- quicklisp-doctor.lisp
+++ quicklisp-doctor.lisp
@@ -4,41 +4,42 @@
 
 ;; (run-program "/usr/bin/hostname" (list))
 ;; (run-program "/usr/bin/which" (list "git"))
-(defun run-program (program args)
+(defun run-program (program-and-args)
   (let ((process (sb-ext:run-program
-                  program
-                  args
-                  :output :stream :wait nil))
+                  (first program-and-args)
+                  (rest  program-and-args)
+                  :output :stream
+                  :wait nil))
         (program-output (make-array '(0) :element-type 'base-char
-                                    :fill-pointer 0 :adjustable t)))
-
+                                         :fill-pointer 0 :adjustable t)))
     (with-output-to-string (s program-output)
       (loop for line = (read-line (sb-ext:process-output process)
                                   nil nil)
             while line
-            do (format s "~a~%" line))
+            do (format s "~&~a" line))
       (sb-ext:process-close process)
 
-      (list :output program-output
-            :exit-code (sb-impl::process-exit-code process)))))
+      (list
+        (sb-impl::process-exit-code process)
+        program-output))))
 
 (defun print-relevant-info ()
-  (format t "OS =========================~%")
+  (format t "OS *************************~%")
   (format t "OS ~S~%" (uiop/os:operating-system))
   (format t "architecture ~S~%" (uiop/os:architecture))
 
-  (format t "quicklisp =========================~%")
+  (format t "quicklisp *************************~%")
   (format t "home ~S~%" ql:*quicklisp-home*)
   (format t "client version ~S~%" (ql:client-version))
   (format t "dist version ~S~%" (ql:dist-version "quicklisp"))
-  (format t "latest dist version ~S~%" (car (ql:available-dist-versions "quicklisp")))
+  (format t "latest dist version ~S~%" (caar (ql:available-dist-versions "quicklisp")))
   (format t "local projects ~S~%" ql:*local-project-directories*)
 
-  (format t "git =========================~%")
+  (format t "git *************************~%")
   (format t "git path ~S~%"
-          (run-program "/usr/bin/which" '("git")))
+          (run-program '("/usr/bin/which" "git")))
   (format t "git version ~S~%"
-          (run-program "/usr/bin/git" '("--version")))
+          (run-program '("/usr/bin/git" "--version")))
   )
 
 (defun examine-declaration (declaration-file)
