4a8fc76ee8784fbbbf1d4fd390bc823bbb816bd3
Jacek Podkanski <ruby.object@googlemail.com>
2022-11-04 22:03:30 +0000

    better exdample

--- check-local-projects.lisp
+++ check-local-projects.lisp
@@ -8,9 +8,10 @@
 (in-package :quicklisp-doctor)
 
 (format t "~S~%"
-        (examine-local-projects '(((:name "cl-confidence"
-                                    :commit "commit 14dc40a0fde822f4aab4fefc9b40ef5fdcbcd666"
-                                    :remote "git@github.com:melusina-org/cl-confidence.git")
-                                   (:name "dot-cons-tree"
-                                    :commit "commit 529dc869e6e46535f4d5567658d750068e55bd5bZZ"
-                                    :remote "git@github.com:bigos/dot-cons-tree.git")))))
+        (quicklisp-doctor:examine-local-projects
+         '((:name "cl-confidence"
+            :commit "commit 14dc40a0fde822f4aab4fefc9b40ef5fdcbcd666"
+            :remote "git@github.com:melusina-org/cl-confidence.git")
+           (:name "dot-cons-tree"
+            :commit "commit 529dc869e6e46535f4d5567658d750068e55bd5bZZ"
+            :remote "git@github.com:bigos/dot-cons-tree.git"))))
