b26b8c358f286002606f6e9abb20f6d794e05109
Jacek Podkanski <ruby.object@googlemail.com>
2022-11-04 15:35:48 +0000

    pachage refinement

--- packages.lisp
+++ packages.lisp
@@ -4,4 +4,5 @@
   (:documentation "Utility for checking quicklisp and local-projects")
   (:use :cl :esrap)
   (:export #:examine-declaration
+           #:examine-local-projects
            #:describe-workstation))
